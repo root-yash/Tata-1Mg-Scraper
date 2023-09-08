6f0c8e18640a21fe766be0b9ebb91b65766867c5
yash goel <yashgoel236437@gmail.com>
2023-09-08 16:49:17 +0530

    add extra information to scrap

--- Src/get_1mg_data.py
+++ Src/get_1mg_data.py
@@ -1,13 +1,16 @@
 from config import Config
 from utils import JsonFunction
-import os, asyncio
+import os
+import asyncio
 from tqdm import tqdm
 from ratelimiter import RateLimiter
 from bs4 import BeautifulSoup
 from urllib import parse
 import requests
 import nest_asyncio
+
 nest_asyncio.apply()
+
 
 class Get1MgData:
     def __init__(self):
@@ -21,30 +24,6 @@ class Get1MgData:
         print("________________Getting Information____________")
         asyncio.get_event_loop().run_until_complete(self.get_details(self.links))
 
-    @RateLimiter(max_calls=1, period=1)
-    async def get_drug_detail(self, link) -> dict:
-        """
-        return Dictionary containing drug details
-        :param link: link of drug page
-        :return: dict with drug details
-        """
-        drug_detail = {}
-        try:
-            header = {"user-agent": "Mozilla/4.0 (compatible; MSIE 6.0; Windows 98) XX"}
-            parsed_html = BeautifulSoup(requests.get(link, headers=header).text, 'html.parser')
-
-            composition = parsed_html.find("div", {"class": "saltInfo DrugHeader__meta-value___vqYM0"})
-            composition = composition.find('a', href=True).text
-
-            drug_detail.update({
-                "composition": composition
-            })
-
-        except:
-            self.data_dict.update({"composition": None})
-
-
-        return drug_detail
 
     async def get_details(self, links: list) -> None:
         """
@@ -90,7 +69,7 @@ class Get1MgData:
                 # update link list
                 JsonFunction.save_data(
                     {
-                        Config.json_link_name: links[idx+1:]
+                        Config.json_link_name: links[idx + 1:]
                     },
                     Config.json_link_output
                 )
@@ -102,4 +81,84 @@ class Get1MgData:
                     Config.data_location
                 )
 
+    @RateLimiter(max_calls=1, period=1)
+    async def get_drug_detail(self, link) -> dict:
+        """
+        return Dictionary containing drug details
+        :param link: link of drug page
+        :return: dict with drug details
+        """
+        drug_detail = {}
+        header = {"user-agent": "Mozilla/4.0 (compatible; MSIE 6.0; Windows 98) XX"}
+        parsed_html = BeautifulSoup(requests.get(link, headers=header).text, 'html.parser')
+
+        # tata 1mg name
+        item_name = (self.__get_text(parsed_html, "h1", "ProductTitle__product-title___3QMYH")
+                     or
+                     self.__get_text(parsed_html, "h1", "DrugHeader__title-content___2ZaPo"))
+
+
+        # Item Company
+        company_name = (self.__get_text(parsed_html, "div", "ProductTitle__manufacturer___sTfon")
+                        or
+                        self.__get_text(parsed_html, "div", "DrugHeader__meta-value___vqYM0"))
+
+        # composition of drug
+        composition = self.__get_href_text(parsed_html, "div", "saltInfo DrugHeader__meta-value___vqYM0")
+
+        # selling price
+        selling_price = self.__get_text(
+            parsed_html,
+            "span",
+            "PriceBoxPlanOption__offer-price___3v9x8 PriceBoxPlanOption__offer-price-cp___2QPU_"
+        ) or self.__get_text(
+            parsed_html,
+            "span",
+            "DrugPriceBox__best-price___32JXw"
+        )
+
+        # MRP
+        mrp = self.__get_text(
+            parsed_html,
+            "span",
+            "PriceBoxPlanOption__margin-right-4___2aqFt PriceBoxPlanOption__stike___pDQVN"
+        ) or self.__get_text(
+            parsed_html,
+            "span",
+            "DrugPriceBox__slashed-price___2UGqd"
+        ) or self.__get_text(
+            parsed_html,
+            "div",
+            "PriceDetails__mrp-tag___3WdTI"
+        )
+
+        # aggregate result
+        drug_detail.update({
+            "item_name": item_name,
+            "item_company": company_name,
+            "composition": composition,
+            "selling_price": selling_price,
+            "mrp": mrp
+        })
+
+        return drug_detail
+
+    def __get_href_text(self, parsed_html, tag: str, class_value: str):
+        try:
+            parsed_tag = parsed_html.find(tag, {"class": class_value})
+            value = parsed_tag.find('a', href=True).text
+
+        except:
+            value = None
+        return value
+
+    def __get_text(self, parsed_html, tag: str, class_value: str):
+        try:
+            value = parsed_html.find(tag, {
+                "class": class_value
+            }).text
+
+        except:
+            value = None
+        return value
 
